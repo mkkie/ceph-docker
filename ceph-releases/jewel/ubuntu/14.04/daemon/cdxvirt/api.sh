99ef8cc67cadffa50e2cc0e8c63b6327cb20faae
Cooper Su <su.jming@gmail.com>
2017-03-21 16:32:23 +0800

    Fix ceph-api & Add api for updating ceph conf

--- ceph-releases/jewel/ubuntu/14.04/daemon/cdxvirt/api.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/cdxvirt/api.sh
@@ -8,7 +8,7 @@ function ceph_api {
   case $1 in
     start_all_osds|set_max_osd|get_max_osd|stop_all_osds|restart_all_osds|get_active_osd_nums|run_osds)
       source cdxvirt/osd.sh
-      osd_controller_env
+      check_docker_cmd
       $@
       ;;
     set_max_mon|get_max_mon|remove_mon)
@@ -18,6 +18,9 @@ function ceph_api {
       ;;
     get_osd_map)
       source cdxvirt/dev-map.sh
+      $@
+      ;;
+    get_ceph_admin|get_ceph_conf)
       $@
       ;;
     *)
