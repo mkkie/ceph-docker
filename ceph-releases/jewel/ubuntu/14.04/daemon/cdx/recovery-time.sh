5445389f289c9d01cafcbae929c384f9b238c1c0
Cooper Su <su.jming@gmail.com>
2017-11-10 17:20:57 +0800

    Accelerate the execution time of ceph-api osd_overview

--- ceph-releases/jewel/ubuntu/14.04/daemon/cdx/recovery-time.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/cdx/recovery-time.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 function cacl_recovery_time {
-  local LEFT_OBJ=$(ceph "${CLI_OPTS[@]}" status -f json | jq .pgmap.degraded_objects)
-  local REC_OBJ_SEC=$(ceph "${CLI_OPTS[@]}" status -f json | jq .pgmap.recovering_objects_per_sec)
+  local CEPH_STAT=$(ceph "${CLI_OPTS[@]}" status -f json)
+  local LEFT_OBJ=$(echo "${CEPH_STAT}" | jq .pgmap.degraded_objects)
+  local REC_OBJ_SEC=$(echo "${CEPH_STAT}" | jq .pgmap.recovering_objects_per_sec)
 
   if [ "${LEFT_OBJ}" == "null" ] || [ "${REC_OBJ_SEC}" == "null" ]; then
     local est_time=0
