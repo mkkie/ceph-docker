46acf14c643724b4c494464fa01b87d606c177f3
Irake <irake99@gmail.com>
2018-02-26 11:17:40 +0800

    'mon-liveprobe.sh' supports multiple IP addresses

--- ceph-releases/jewel/ubuntu/14.04/daemon/cdx/mon-liveprobe.sh
+++ ceph-releases/jewel/ubuntu/14.04/daemon/cdx/mon-liveprobe.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 set -e
 
-MON_IP=$(ip -4 -o a | awk '{ sub ("/..", "", $4); print $4 }' | grepcidr "${CEPH_PUBLIC_NETWORK}")
-MON_PORT=6789
+MON_IPS="$(ip -4 -o a | awk '{ sub ("/..", "", $4); print $4 }' | grepcidr "${CEPH_PUBLIC_NETWORK}")"
+MON_PORT="6789"
 
-if ! curl "$MON_IP":"$MON_PORT"; then
-  echo " dial tcp $MON_IP:$MON_PORT: getsockopt: connection refused"
+for MON_IP in ${MON_IPS}; do
+  if curl -s "${MON_IP}":"${MON_PORT}" > /dev/null; then
+    HEALTHY="true"
+    break
+  else
+    echo " dial tcp ${MON_IP}:${MON_PORT}: getsockopt: connection refused" 1>&2
+  fi
+done
+
+if [[ "${HEALTHY}" == "true" ]]; then
+  exit 0
+else
   exit 1
 fi
